36adfbb4336955ebd10c5b3156712c16b176f4f4
Fig Yogurt <itisaftab@gmail.com>
2020-11-08 16:48:42 -0500

    fix(app): removed unnecessary import and included a little more info on the useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import currency from "currency.js";
 import "./App.css";
